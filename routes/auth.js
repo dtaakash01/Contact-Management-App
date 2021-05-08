50ab20b3248f32b8179954d2a9c60324e52f15ba
Akash Dixit <akashdixit@localhost.localdomain>
2021-05-08 19:55:42 +0530

    middleware added

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const { body, validationResult } = require('express-validator');
 const config = require('config');
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs');
+const auth = require('../middleware/auth');
 
 
 const User = require('../models/users')
@@ -12,8 +13,16 @@ const User = require('../models/users')
 // @desc Register user
 // @access Private
 
-router.get('/', (req, res) => {
-    res.send("Get logged in user")
+router.get('/',auth, async (req, res) => {
+   try {
+       
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user)
+
+   } catch (err) {
+       console.error(err.message);
+       res.status(500).send("Server Error")
+   }
 });
 
 

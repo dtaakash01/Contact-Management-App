5e373a291883f92ab73b3e266314155d9369cf0d
Akash Dixit <akashdixit@localhost.localdomain>
2021-05-13 15:43:46 +0530

    Login Logout navbar changes login auth updated

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { Fragment,useContext } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/Auth/authContext'
 
 
 const Navbar = ({title, icon}) => {
+
+    const authContext = useContext(AuthContext);
+
+    const {isAuthenticated, user, logout} = authContext;
+
+    const onLogout = () => {
+        logout();
+    }
+
+    const authLinks = (
+        <Fragment>
+            <li>Hello {user && user.name}</li>
+            {' '}
+            <li>
+                <a onClick={onLogout} href='!#'>
+                    <i className="fas fa-sign-out-alt"></i> <span className="hide-sm">Logout</span>
+                </a>
+            </li>
+        </Fragment>
+    )
+
+    const guestLinks = (
+        <Fragment>
+                <li>
+                    <Link to = '/about'>About</Link>
+                </li>
+                 <li>
+                    <Link to = '/register'>Register</Link>
+                </li>
+
+                <li>
+                    <Link to = '/login'>Login</Link>
+                </li>
+        </Fragment>
+    )
+
     return (
         <div className='navbar bg-primary'>
             <h1>
@@ -12,21 +49,9 @@ const Navbar = ({title, icon}) => {
             </h1>
 
             <ul>
-                <li>
-                    <Link to = '/'>Home</Link>
-                </li>
-          
-                <li>
-                    <Link to = '/about'>About</Link>
-                </li>
 
-                <li>
-                    <Link to = '/register'>Register</Link>
-                </li>
-
-                <li>
-                    <Link to = '/login'>Login</Link>
-                </li>
+                {isAuthenticated ? authLinks : guestLinks}
+               
             </ul>
         </div>
     )
